bf96db238c7c2fc5a81ecd0b6f897d763cab8fa6
Adam <you@example.com>
2023-12-21 20:50:34 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// const firstName = 'Adam'
+// firstName = 'Pavel'
+// console.log('Hello', firstName)
+
+let firstName = 'Adam'
+firstName = 'Pavel'
+
+console.log('Hello', firstName)
+
+var lastName = 'Adam'
+lastName = 'Shamil'
+
+console.log(lastName)
